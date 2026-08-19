c1062005036204dbb1c01927fcfefacfe0d29713
rothwegk <sunrisecitrus.admin@gmail.com>
2026-08-19 17:20:52 -0400

    fix catch-all route

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.post('/api/send-invoice', async (req, res) => {
 })
 
 // Fallback to frontend for any other route
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
 
