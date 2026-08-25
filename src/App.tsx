bdcbc8dcb44db3868a66efd6b1d8b20671f111c0
rothwegk <sunrisecitrus.admin@gmail.com>
2026-08-25 16:39:42 -0400

    add login and protect all routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { Session } from '@supabase/supabase-js'
+import type { Session } from '@supabase/supabase-js'
 import { supabase } from './lib/supabase'
 import Layout from './components/Layout'
 import Dashboard from './pages/Dashboard'
