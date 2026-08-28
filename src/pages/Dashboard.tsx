d4b4b6ae0d3221cc7aa29a3e4a5eaf3400a3d767
rothwegk <sunrisecitrus.admin@gmail.com>
2026-08-28 12:31:33 -0400

    Fix supabase import path

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { supabase } from '../supabaseClient'
+import { supabase } from '../lib/supabase'
 import { Link } from 'react-router-dom'
 
 export default function Dashboard() {
