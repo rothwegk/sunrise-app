cda0b3c83c73c9c67c61970b8c6054503361c98f
rothwegk <sunrisecitrus.admin@gmail.com>
2026-08-28 12:27:31 -0400

    Fix routing syntax error

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ import Estimates from './pages/Estimates'
 import Settings from './pages/Settings'
 import Calendar from './pages/Calendar'
 import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
 
 function ProtectedRoute({ children }: { children: React.ReactNode }) {
   const [session, setSession] = useState<Session | null>(null)
@@ -52,7 +51,6 @@ export default function App() {
         <Route path="/login" element={<Login />} />
 
         <Route
-          <Route path="/" element={<Dashboard />} />
           element={
             <ProtectedRoute>
               <Layout />
